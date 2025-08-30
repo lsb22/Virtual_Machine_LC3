470a73b04c8bc8a689b2cd24dab6237ac28ddafe
lsb <likith15600@gmail.com>
2025-08-30 20:12:20 +0530

    add logic for STI opcode

--- main.c
+++ main.c
@@ -294,7 +294,13 @@ int main(int argc, const char *argv[])
             break;
         }
         case OP_STI:
+        {
+            // STI -> store indirect
+            uint16_t r0 = (instr >> 9) & 0x7;
+            uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+            mem_write(mem_read(reg[R_PC] + pc_offset), reg[r0]);
             break;
+        }
         case OP_STR:
             break;
         case OP_TRAP:
