1fd9a85a43dad2fad56dcc3829a9ce30e2c61f3d
lsb <likith15600@gmail.com>
2025-08-30 20:02:27 +0530

    add logic for LEA opcode

--- main.c
+++ main.c
@@ -277,7 +277,14 @@ int main(int argc, const char *argv[])
             break;
         }
         case OP_LEA:
+        {
+            // LEA -> Load Effective Address
+            uint16_t r0 = (instr >> 9) & 0x7;
+            uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+            reg[r0] = reg[R_PC] + pc_offset;
+            update_flags(r0);
             break;
+        }
         case OP_ST:
             break;
         case OP_STI:
