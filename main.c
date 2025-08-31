956f78e46c101dfcde2f1c59c243c1f01871d48c
lsb <likith15600@gmail.com>
2025-08-31 13:07:06 +0530

    add logic for trap code IN

--- main.c
+++ main.c
@@ -353,7 +353,15 @@ int main(int argc, const char *argv[])
                 break;
             }
             case TRAP_IN:
+            {
+                printf("Enter a character: ");
+                char c = getchar();
+                putc(c, stdout);
+                fflush(stdout);
+                reg[R_R0] = (uint16_t)c;
+                update_flags(R_R0);
                 break;
+            }
             case TRAP_PUTSP:
                 break;
             case TRAP_HALT:
